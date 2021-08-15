a84d8f1b7c6acf5f6ec2dc17ff54af6b996bd9e2
MechEngMalik <mecheng.malik93@gmail.com>
2021-08-15 21:39:03 +0300

    f

--- city-explorer/src/App.js
+++ city-explorer/src/App.js
@@ -1,57 +1,78 @@
 
 import React from 'react';
 import axios from 'axios';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Form from 'react-bootstrap/Form'
+import Image from 'react-bootstrap/Image'
+import Button from 'react-bootstrap/Button'
+import "./App.css";
 
 class App extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state={
+    this.state = {
       cityInfo: {},
-      findCity :'',
-      show : false
-    } 
-  }
-  
+      findCity: '',
+      show: false,
 
-  searchCity= async(event)=>{
+    }
+  }
+
+
+
+  searchCity = async (event) => {
     event.preventDefault();
-console.log(event.target.city.value)
-    this.setState({
+    // console.log(event.target.city.value)
+    await this.setState({
       findCity: event.target.city.value
-      
+
     })
 
 
 
-    let locUrl=`https://eu1.locationiq.com/v1/search.php?key=pk.87bf99edc4a08e1d82b554684aab91db&q=amman&format=json`
+    let locUrl = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.findCity}&format=json`
+
     let selectedCity = await axios.get(locUrl)
 
 
     this.setState({
       cityInfo: selectedCity.data[0],
       show: true
+      
     })
 
 
 
   }
-  
-  render(){
-    return(
+
+
+
+  render() {
+    return (
       <div>
         <h2> City Explorer</h2>
-        <form onSubmit= {this.searchCity}>
-          <input type="text" placeholder="insert the wanted city" name="city" />
-          <button>Find the City</button>
-        </form>
+        <Form onSubmit={this.searchCity}>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label>CITY NAME</Form.Label>
+            <Form.Control type="text" placeholder="insert the wanted city" name="city" />
+          </Form.Group>
 
-        {this.state.show && <p>lat:{this.state.cityInfo.lat}<br></br>lon:{this.state.cityInfo.lon}</p>}
+          <Button type='submit'>Find the City</Button>
+        </Form>
+
+
+        {this.state.show && <p>{this.state.findCity}<br></br>lat:{this.state.cityInfo.lat}<br></br>lon:{this.state.cityInfo.lon}</p>}
+        
+        
+        {this.state.show &&
+          <Image
+            src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${this.state.cityInfo.lat},${this.state.cityInfo.lon}&zoom=14&size=400x400&markers=icon:large-red-cutout|${this.state.cityInfo.lat},${this.state.cityInfo.lon}|${this.state.cityInfo.lat},${this.state.cityInfo.lon}`} fluid />}
 
       </div>
     )
   }
 
-  
+
 }
 export default App;
