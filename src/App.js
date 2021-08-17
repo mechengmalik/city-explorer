1049b021e0d388fb6a61cdbc1446adfd5864569f
MechEngMalik <mecheng.malik93@gmail.com>
2021-08-17 18:03:22 +0300

    finish the lab

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Form from 'react-bootstrap/Form'
 import Image from 'react-bootstrap/Image'
 import Button from 'react-bootstrap/Button'
 import "./App.css";
+import Weather from './components/Weather';
 
 class App extends React.Component {
 
@@ -13,6 +14,8 @@ class App extends React.Component {
     super(props);
     this.state = {
       cityInfo: {},
+      lat: "",
+      lon: "",
       weatherInfo: [],
       findCity: '',
       show: false,
@@ -24,32 +27,52 @@ class App extends React.Component {
 
   searchCity = async (event) => {
     event.preventDefault();
-    // console.log(event.target.city.value)
+    console.log(event.target.city.value)
     await this.setState({
       findCity: event.target.city.value
 
     })
 
+    console.log(this.state.findCity)
+ 
 
+    let locUrl = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${event.target.city.value}&format=json`
 
-    let locUrl = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.findCity}&format=json`
-
+    try{
     let selectedCity = await axios.get(locUrl)
-
-
-
-    let LocUrl2 = ` ${process.env.REACT_APP_SERVER_LINK}/weather?&lat=latitude&lon=lontitude&searchQuery=${this.state.findCity}`
-
-    let selectedWether = await axios.get(LocUrl2)
-
-
-
     this.setState({
-      cityInfo: selectedCity.data[0],
-      weatherInfo: selectedWether.data,
-      show: true
+     cityInfo: selectedCity.data[0],
+     lat: selectedCity.data[0].lat,
+     lon: selectedCity.data[0].lon,
+     show: true
+  
+   })
 
+  }catch(error){
+    console.log(error.response)
+
+
+  }
+
+
+
+
+    let LocUrl2 =`${process.env.REACT_APP_SERVER_LINK}/weather?searchQuery=${this.state.findCity}&lat=${this.state.lat}&lon=${this.state.lon}`
+    try{
+    let selectedWether = await axios.get(LocUrl2)
+    await this.setState({
+      weatherInfo:selectedWether.data
     })
+    console.log(selectedWether)
+
+  }catch(error){
+      console.log(error.response)
+
+
+    }
+
+
+
 
 
 
@@ -60,15 +83,22 @@ class App extends React.Component {
   render() {
     return (
       <div>
+
+
         <h2> City Explorer</h2>
+
+
         <Form onSubmit={this.searchCity}>
           <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
             <Form.Label>CITY NAME</Form.Label>
             <Form.Control type="text" placeholder="insert the wanted city" name="city" />
           </Form.Group>
 
+
+
           <Button type='submit'>Find the City</Button>
         </Form>
+
 
 
         {this.state.show && <p>{this.state.findCity}<br></br>lat:{this.state.cityInfo.lat}<br></br>lon:{this.state.cityInfo.lon}</p>}
@@ -79,12 +109,15 @@ class App extends React.Component {
             src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${this.state.cityInfo.lat},${this.state.cityInfo.lon}&zoom=14&size=400x400&markers=icon:large-red-cutout|${this.state.cityInfo.lat},${this.state.cityInfo.lon}|${this.state.cityInfo.lat},${this.state.cityInfo.lon}`} fluid />}
 
 
+        <Weather
+          show={this.state.show}
+          data={this.state.weatherInfo} />
 
-{this.state.show && <p>{this.state.findCity}<br></br>lat:{this.state.cityInfo.lat}<br></br>lon:{this.state.cityInfo.lon}</p>}
-        <p>day {i}</p>
 
 
-      </div
+
+
+      </div >
     )
   }
 
---
+++ src/components/Weather.js
@@ -0,0 +1,33 @@
+
+import React from 'react';
+import { Card } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+class Weather extends React.Component{
+
+    render(){
+        return(
+            <div>
+                {this.props.show &&
+                this.props.data.map((item,i)=>{
+                    return(
+                        <Card key={i} style= {{width :'20rem'}}>
+                            <Card.Body>
+
+                                <Card.Text>date: {item.date}</Card.Text>
+                                <Card.Text>description: {item.description}</Card.Text>
+
+                            </Card.Body>
+                        </Card>
+                    )
+                })
+            }
+            </div>
+        )
+    }
+
+}
+
+
+export default Weather;
